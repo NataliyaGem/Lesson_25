67a12a4de27a478f1d48f6b09654d39d9216314d
nata@nata <n426442@gmail.com>
2024-03-26 10:19:10 +0300

    вывод в консоль с пунктиров

--- app.rb
+++ app.rb
@@ -5,5 +5,11 @@ db = SQLite3::Database.new 'n.db'
 #db.execute "INSERT INTO n (Name, Phone) VALUES ('Valera', 17823)"
 #db.execute "INSERT INTO n (Name, Phone) VALUES ('Roman', 89347)"
 #db.execute "INSERT INTO n (Name, Phone) VALUES ('Filipp', 12385)"
+#вывод в консоди с пунктирными линиями
+db.execute "SELECT * FROM n" do |n|
+  puts n
+  puts "====="
+end
+
 
 db.close
